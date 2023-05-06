aea65b3f0065c9390021241c2fba95b62b4edbee
Goitse-Hub <121558821+Goitse-Hub@users.noreply.github.com>
2023-05-06 13:44:17 +0200

    server done, waiting for client

---
+++ server/app.js
@@ -0,0 +1,109 @@
+require("dotenv").config();
+require("./config/database").connect();
+const express = require("express");
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.use(express.json());
+
+// Logic goes here
+// importing user context
+const User = require("./model/user");
+
+const auth = require("./middleware/auth");
+
+app.post("/welcome", auth, (req, res) => {
+  res.status(200).send("Welcome 🙌 ");
+});
+
+// Register
+app.post("/register", async (req, res) => {
+// our register logic goes here...
+ // Our register logic starts here
+ try {
+    // Get user input
+    const { first_name, last_name, email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password && first_name && last_name)) {
+      res.status(400).send("All input is required");
+    }
+
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+
+    //Encrypt user password
+    encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await User.create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      password: encryptedPassword,
+    });
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.JWT_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+    // save user token
+    user.token = token;
+
+    // return new user
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Login
+app.post("/login", async (req, res) => {
+// our login logic goes here
+// Our login logic starts here
+try {
+    // Get user input
+    const { email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate if user exist in our database
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.JWT_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      // save user token
+      user.token = token;
+
+      // user
+      res.status(200).json(user);
+    }
+    res.status(400).send("Invalid Credentials");
+  } catch (err) {
+    console.log(err);
+  }
+  // Our register logic ends here
+});
+
+module.exports = app;
--- server/app/config/db.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    
-        url: `mongodb+srv://groupcfs:12345@cluster0.97amkdg.mongodb.net/?retryWrites=true&w=majority`
-      
-  };
